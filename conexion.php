738d71b6438e60e7cf5b74d26eb8ba99b763fd81
Juan Sebastian Cruz Perdomo <jscp123456@hotmail.com>
2013-09-27 14:27:11 -0500

    ahora si va el procedimiento

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
-<?php 
+﻿<?php 
 $Servidor='localhost';//Nombre del servidor
-$Usuario='gridsoa_admin';//Nombre del Usuario
-$Password='Asistentic123';//Contraseña del Usuario
+$Usuario='root';//Nombre del Usuario
+$Password='';//Contraseña del Usuario
 $BaseDatos='gridsoa_asistentic';//Base de datos
 $Copia='C:\AppServ\MySQL\bin\mysqldump.exe';
 $Restore='c:\AppServ\MySQL\bin\mysql.exe';
